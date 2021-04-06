d2fb4ada495f199e04f8375f45e45b38bab924c5
Ximena <ximena.andrade@symplifica.com>
2021-04-06 15:32:39 -0500

    Follow: Model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,16 @@ class User < ApplicationRecord
   has_many :like_posts
   has_many :_like_posts, through: :like_posts, class_name: "Post", source: :post
   has_many :comments
+
+  # Will return an array of follows for the given user instance
+  has_many :received_follows, foreign_key: :followed_id, class_name: "Follow"
+
+  # Will return an array of users who follow the user instance
+  has_many :followers, through: :received_follows, source: :follower
+
+  # returns an array of follows a user gave to someone else
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+
+  # returns an array of other users who the user has followed
+  has_many :followings, through: :given_follows, source: :followed
 end
