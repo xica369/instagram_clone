dd7a6c2737a3b99bcada3555991f3897c448a758
Ximena <ximena.andrade@symplifica.com>
2021-04-09 23:42:01 -0500

    [views] Application - layouts

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module InstagramClone
     config.generators do |g|
       g.template_engine :slim
     end
+
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
+
   end
 end
