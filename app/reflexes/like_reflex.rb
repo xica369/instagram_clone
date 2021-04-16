a45807bc044923ea778892bdb7dd1afe40a03f8f
Ximena <ximena.andrade@symplifica.com>
2021-04-16 09:57:33 -0500

    [specs] Likes and profile views

--- app/reflexes/like_reflex.rb
+++ app/reflexes/like_reflex.rb
@@ -8,16 +8,10 @@ class LikeReflex < ApplicationReflex
     @icon = false
 
     if user_like.nil?
-      puts ">" * 50
-      p post.like_posts.create(user:user)
       @icon = true
     else
-      puts "%" * 50
       LikePost.delete(user_like)
     end
-
-    puts "\n" * 5
-    p user
 
   end
 
--- spec/features/instagram_spec.rb
+++ spec/features/instagram_spec.rb
@@ -13,13 +13,10 @@ RSpec.feature "Instagrams", type: :feature do
     fill_in 'user_password', with: @user.password
     click_button "Log In"
     expect(page).to have_current_path(root_path)
-    # sleep(10)
+    # sleep(2)
   end
   it "Comment" do
     @post = create(Post.model_name.singular.to_sym, user:@user)
-    puts ">" * 30
-    p User.all.count
-    p Post.all.count
     visit new_user_session_path
     fill_in 'user_email', with: @user.email
     fill_in 'user_password', with: @user.password
@@ -30,20 +27,60 @@ RSpec.feature "Instagrams", type: :feature do
     visit current_path
     expect(page).to have_content('pruebasss')
     expect(page).to have_content('xime')
-
+    # sleep(2)
   end
   it "Register" do
     visit new_user_registration_path
-    # fill_in "user_email", with: Faker::Internet.email
-    # fill_in "user_name", with: Faker::FunnyName.four_word_name
-    # fill_in "user_username", with: Faker::FunnyName.four_word_name
-    # fill_in "user_password", with: Faker::Internet.password
-    # click_button "Sign up"
-    fill_in "user_email", with: "user@user.com"
-    fill_in "user_name", with: "user"
-    fill_in "user_username", with: "user_name"
-    fill_in "user_password", with: "123456"
+    fill_in "user_email", with: Faker::Internet.email
+    fill_in "user_name", with: Faker::FunnyName.four_word_name
+    fill_in "user_username", with: Faker::FunnyName.four_word_name
+    fill_in "user_password", with: Faker::Internet.password
     click_button "Sign up"
+    # fill_in "user_email", with: "user@user.com"
+    # fill_in "user_name", with: "user"
+    # fill_in "user_username", with: "user_name"
+    # fill_in "user_password", with: "123456"
+    # click_button "Sign up"
     # sleep(20)
+  end
+  it "Likes" do
+    @post = create(Post.model_name.singular.to_sym, user:@user)
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: @user.password
+    click_button "Log In"
+    expect(page).to have_current_path(root_path)
+    click_button "like_button"
+    visit current_path
+    expect(page).to have_content('1 likes')
+    # sleep(2)
+    click_button "like_button"
+    visit current_path
+    expect(page).to have_content('0 likes')
+    # sleep(2)
+  end
+  it "Profile view" do
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: @user.password
+    click_button "Log In"
+    expect(page).to have_current_path(root_path)
+    find(:css, 'i.far.fa-user').click
+    find_link('Profile').click
+    expect(page).to have_current_path(profile_index_path)
+    # sleep(5)
+  end
+  it "Profile - home view" do
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: @user.password
+    click_button "Log In"
+    expect(page).to have_current_path(root_path)
+    find(:css, 'i.far.fa-user').click
+    find_link('Profile').click
+    expect(page).to have_current_path(profile_index_path)
+    find(:css, 'i.far.fa-home').click
+    expect(page).to have_current_path(root_path)
+    # sleep(5)
   end
 end
